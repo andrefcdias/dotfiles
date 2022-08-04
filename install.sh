597e2cfec09c3807c056b799e386e34725ef0b4b
André <39736248+andrefcdias@users.noreply.github.com>
2022-08-04 12:52:37 +0200

    strip down unused stuff

--- install.sh
+++ install.sh
@@ -3,11 +3,3 @@
 exec > >(tee -i $HOME/dotfiles_install.log)
 exec 2>&1
 set -x
-
-ln -s $(pwd)/tmux.conf $HOME/.tmux.conf
-ln -s $(pwd)/vimrc $HOME/.vimrc
-ln -s $(pwd)/vim $HOME/.vim
-ln -s $(pwd)/emacs $HOME/.emacs
-ln -s $(pwd)/screenrc $HOME/.screenrc
-
-vim -Es -u $HOME/.vimrc -c "PlugInstall | qa"
