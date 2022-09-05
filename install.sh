570e2819a7a50d60594bb87b9748467e09487d83
André Dias <39736248+andrefcdias@users.noreply.github.com>
2022-09-05 20:17:15 +0200

    add mac font instalation

--- install.sh
+++ install.sh
@@ -13,10 +13,15 @@ git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_C
 git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone --depth=1 https://github.com/romkatv/powerlevel10k ${ZSH_CUSTOM:-~/.oh-my-zsh}/themes/powerlevel10k
 
-curl https://raw.githubusercontent.com/romkatv/powerlevel10k-media/master/MesloLGS%20NF%20Regular.ttf --output ~/.local/share/fonts/meslo-lgs-nf/regular.ttf --create-dirs
-curl https://raw.githubusercontent.com/romkatv/powerlevel10k-media/master/MesloLGS%20NF%20Bold.ttf --output ~/.local/share/fonts/meslo-lgs-nf/bold.ttf --create-dirs
-curl https://raw.githubusercontent.com/romkatv/powerlevel10k-media/master/MesloLGS%20NF%20Italic.ttf --output ~/.local/share/fonts/meslo-lgs-nf/italic.ttf --create-dirs
-curl https://raw.githubusercontent.com/romkatv/powerlevel10k-media/master/MesloLGS%20NF%20Bold%20Italic.ttf --output ~/.local/share/fonts/meslo-lgs-nf/bolditalic.ttf --create-dirs
+curl https://raw.githubusercontent.com/romkatv/powerlevel10k-media/master/MesloLGS%20NF%20Regular.ttf --output /tmp/meslo_font/MesloLGS_NF_Regular.ttf --create-dirs
+curl https://raw.githubusercontent.com/romkatv/powerlevel10k-media/master/MesloLGS%20NF%20Bold.ttf --output /tmp/meslo_font/MesloLGS_NF_Bold.ttf --create-dirs
+curl https://raw.githubusercontent.com/romkatv/powerlevel10k-media/master/MesloLGS%20NF%20Italic.ttf --output /tmp/meslo_font/MesloLGS_NF_Italic.ttf --create-dirs
+curl https://raw.githubusercontent.com/romkatv/powerlevel10k-media/master/MesloLGS%20NF%20Bold%20Italic.ttf --output /tmp/meslo_font/MesloLGS_NF_BoldItalic.ttf --create-dirs
+
+# TODO: If Windows
+
+# If Mac
+[ -d "/Library/Fonts" ] && cp /tmp/meslo_font/* /Library/Fonts/
 
 cp -f $CURR_DIR/.zshrc $HOME/.zshrc
 cp -f $CURR_DIR/.p10k.zsh $HOME/.p10k.zsh
