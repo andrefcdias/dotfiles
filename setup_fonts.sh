85b870ac80c40d713aa6291ac2f9f17ebbd98fab
André Dias <39736248+andrefcdias@users.noreply.github.com>
2022-09-06 11:50:00 +0000

    cleanup setup_fonts

--- setup_fonts.sh
+++ setup_fonts.sh
@@ -1,4 +1,3 @@
-
 curl https://raw.githubusercontent.com/romkatv/powerlevel10k-media/master/MesloLGS%20NF%20Regular.ttf --output /tmp/meslo_font/MesloLGS_NF_Regular.ttf --create-dirs
 curl https://raw.githubusercontent.com/romkatv/powerlevel10k-media/master/MesloLGS%20NF%20Bold.ttf --output /tmp/meslo_font/MesloLGS_NF_Bold.ttf --create-dirs
 curl https://raw.githubusercontent.com/romkatv/powerlevel10k-media/master/MesloLGS%20NF%20Italic.ttf --output /tmp/meslo_font/MesloLGS_NF_Italic.ttf --create-dirs
